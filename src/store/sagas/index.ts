4902040903bd8a6c0da442fa56055fdc7f011696
leduylinh <leduylinh209@gmail.com>
2020-10-08 16:50:58 +0700

    init template

---
+++ src/store/sagas/index.ts
@@ -0,0 +1,7 @@
+import { takeEvery, all } from 'redux-saga/effects';
+import * as types from '../actions/types';
+import loginSaga from './loginSaga';
+
+export default function* watch() {
+    yield all([takeEvery(types.LOGIN_REQUEST, loginSaga)]);
+}
