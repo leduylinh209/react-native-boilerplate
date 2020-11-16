56bf4685cc9663c24ef8761f2c9465bd69bc22e2
leduylinh <leduylinh209@gmail.com>
2020-11-16 09:07:32 +0700

    implement auto generate

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,12 +1,10 @@
 /* SCREEN IMPORT */
-import DogCart from './DogCart';
 import Login from './Login';
 import Home from './Home';
 import ForgotPassword from './ForgotPassword';
 
 export {
     /* SCREEN EXPORT */
-	DogCart,
     Login,
     Home,
     ForgotPassword,
